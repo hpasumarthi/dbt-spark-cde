f6f113dfe4f2f4c9896070947eec83a4a72b342a
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-09-23 19:10:08 +0530

    bump version for pypi release (#40)

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def _get_dbt_core_version():
 package_name = "dbt-spark-cde"
 package_version = "1.3.0"
 dbt_core_version = _get_dbt_core_version()
-description = """The dbt-spark-cde adapter plugin for Spark in Cloudera DataHub with Livy interface"""
+description = """The CDE API based Cloudera Spark adapter plugin for dbt"""
 
 odbc_extras = ["pyodbc>=4.0.30"]
 pyhive_extras = [
